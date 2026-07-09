8e6d8bc12af107c431f94daae5a01a06df0dba41
masterfluid-designer <master.flowuidesign@gmail.com>
2026-07-09 23:09:46 +0000

    boil

---
+++ apps/web/components/Header/menuData.tsx
@@ -0,0 +1,42 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "Accueil",
+    newTab: false,
+    path: "/",
+  },
+  {
+    id: 2,
+    title: "Fonctionnalités",
+    newTab: false,
+    path: "/#features",
+  },
+  {
+    id: 3,
+    title: "Tarifs",
+    newTab: false,
+    path: "/#pricing",
+  },
+  {
+    id: 4,
+    title: "FAQ",
+    newTab: false,
+    path: "/#faq",
+  },
+  {
+    id: 5,
+    title: "Blog",
+    newTab: false,
+    path: "/blog",
+  },
+  {
+    id: 6,
+    title: "Contact",
+    newTab: false,
+    path: "/#contact",
+  },
+];
+
+export default menuData;
